2ac84f788b164df1c539f34468621cf18b95448c
andree2403 <cjmorgan2589@gmail.com>
2023-05-25 16:46:13 +0000

    Task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,38 @@
-#!/usr/bin/python3
+#!/usr/bin/env bash
+# Installs, configures, and starts the web server
 
-apt-get -y update
-apt-get -y install nginx
-ufw allow 'Nginx HTTP'
-mkdir data
-cd data
-mkdir web_static
-cd web_static
-mkdir releases
-mkdir shared
-cd releases
-mkdir test
-cd test
-touch index.html
-echo 'Hello World!' > /data/web_static/releases/test/index.html
-sudo chmod -R 755 /data/
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By \$HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" | sudo tee /etc/nginx/sites-available/default
+sudo service nginx restart
