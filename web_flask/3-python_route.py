31ed08bc9a9ad5a1f45a7cbce5bd62ff4b1b3880
andree2403 <cjmorgan2589@gmail.com>
2023-06-14 17:36:50 +0000

    Task

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application:
+
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+You must use the option strict_slashes=False in your route definition"""
+
+from flask import Flask, render_template
+# Create a flask instamce
+app = Flask(__name__)
+
+
+# Create a route decorator
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    if "_" in text:
+        text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def display_text(text='is cool'):
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
