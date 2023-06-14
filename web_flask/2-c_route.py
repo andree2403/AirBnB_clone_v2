48c97f9aeba09f9b8ef99f2e8b1bec667102dee4
andree2403 <cjmorgan2589@gmail.com>
2023-06-14 17:07:10 +0000

    Task

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -16,15 +16,18 @@ app = Flask(__name__)
 def index():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     if "_" in text:
         text = text.replace("_", " ")
     return "C {}".format(text)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
